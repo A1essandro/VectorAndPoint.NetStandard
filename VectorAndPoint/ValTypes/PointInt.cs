85539cc858218f7a3647a1e54d609d12d9083eae
Yermakov Alexander <alexander.yermakov.1990@gmail.com>
2019-03-26 21:42:24 +0300

    Add PointInt and PointInt3D

---
+++ VectorAndPoint/ValTypes/PointInt.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VectorAndPoint.ValTypes
+{
+    /// <summary>
+    /// Represents an x- and y-coordinates in 2D space.
+    /// </summary>
+    [DebuggerDisplay("({X}, {Y})")]
+    public struct PointInt : I2D<int>, IEquatable<I2D<int>>, IEquatable<PointInt>
+    {
+
+        /// <summary>
+        /// Creates a new VectorAndPoint.ValTypes.PointInt structure that contains the specified coordinates.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public PointInt(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// X-coordinate value of this point
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Y-coordinate value of this point
+        /// </summary>
+        public int Y { get; }
+
+        #region static
+
+        public static double GetRangeBetween(I2D<int> p1, I2D<int> p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        public static double GetRangeBetween(PointInt p1, PointInt p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        public static bool IsEquals(I2D<int> p1, I2D<int> p2) => p1.X == p2.X && p1.Y == p2.Y;
+
+        public static bool IsEquals(PointInt p1, PointInt p2) => p1.X == p2.X && p1.Y == p2.Y;
+
+        #endregion
+
+        #region IEquatable
+
+        public bool Equals(I2D<int> other) => IsEquals(this, other);
+
+        public bool Equals(PointInt other) => IsEquals(this, other);
+
+        #endregion
+
+        #region object overrides
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj is I2D<int>) return Equals(obj as I2D<int>);
+            if (obj is PointInt) return Equals((obj as PointInt?).Value);
+
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode() => (X.GetHashCode() * 11) ^ (Y.GetHashCode() * 7);
+
+        public override string ToString() => $"({X}, {Y})";
+
+        #endregion
+
+        #region operators
+
+        public static Point operator +(PointInt v, Vector p) => new Point(p.X + v.X, p.Y + v.Y);
+
+        #endregion
+
+    }
+}
---
+++ VectorAndPoint/ValTypes/PointInt3D.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VectorAndPoint.ValTypes
+{
+
+    /// <summary>
+    /// Represents an x-, y- and z-coordinates in 3D space.
+    /// </summary>
+    [DebuggerDisplay("({X}, {Y}, {Z})")]
+    public struct PointInt3D : I3D<int>, IEquatable<I3D<int>>, IEquatable<PointInt3D>
+    {
+
+        /// <summary>
+        /// Creates a new VectorAndPoint.ValTypes.PointInt3D structure that contains the specified coordinates.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public PointInt3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// X-coordinate value of this point
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Y-coordinate value of this point
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Z-coordinate value of this point
+        /// </summary>
+        public int Z { get; }
+
+        #region static
+
+        public static double GetRangeBetween(I3D<int> p1, I3D<int> p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2) + Math.Pow(p1.Z - p2.Z, 2));
+        }
+
+        public static double GetRangeBetween(PointInt3D p1, PointInt3D p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2) + Math.Pow(p1.Z - p2.Z, 2));
+        }
+
+        public static bool IsEquals(I3D<int> p1, I3D<int> p2) => p1.X == p2.X && p1.Y == p2.Y;
+
+        public static bool IsEquals(Point p1, Point p2) => p1.X == p2.X && p1.Y == p2.Y;
+
+        public static bool IsCollinear(I3D<int> p1, I3D<int> p2) => p1.X / p2.X == p1.Y / p2.Y;
+
+        #endregion
+
+        #region IEquatable
+
+        public bool Equals(I3D<int> other) => IsEquals(this, other);
+
+        public bool Equals(PointInt3D other) => IsEquals(this, other);
+
+        #endregion
+
+        #region object overrides
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj is I3D<int>) return Equals(obj as I3D<int>);
+            if (obj is PointInt3D) return Equals((obj as PointInt3D?).Value);
+
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode() => (X.GetHashCode() * 11) ^ (Y.GetHashCode() * 7) ^ (Z.GetHashCode() * 5);
+
+        public override string ToString() => $"({X}, {Y}, {Z})";
+
+        #endregion
+
+        #region operators
+
+        public static Point3D operator +(PointInt3D v, Vector3D p) => new Point3D(p.X + v.X, p.Y + v.Y, p.Z + v.Z);
+
+        #endregion
+
+    }
+}
